ed60ebab8201c2e76eab418769b85caad95d6221
Farhad Sadykov <f.sadykov@yahoo.com>
2025-02-02 22:35:18 +0300

    finished header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+function Header() {
+
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-wrap">
+          <a href="#" className="header-logo">
+            Snooker <span>Shots Counter</span>
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
