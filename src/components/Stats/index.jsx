3f65ad6398b02570bda00cd9075f5a3aa1709c42
Farhad Sadykov <f.sadykov@yahoo.com>
2025-02-07 01:13:56 +0300

    edited fonts, Stats component

--- src/components/Stats/index.jsx
+++ src/components/Stats/index.jsx
@@ -1,14 +1,17 @@
 
+
 function Stats() {
   return (
     <section className="stats">
       <div className="container">
-        <div className="stats-pot">
-          <div className="stats-pot-first-player">
-            <span className="stats-pot-first-player-info">
+
+        {/* POT SUCCESS  */}
+        <div className="stats-card">
+          <div className="stats-player">
+            <span className="stats-player-info">
               0% (0/0)
             </span>
-            <div className="stats-pot-first-player-btns">
+            <div className="stats-player-btns">
               <button className="stats-potted-btn">
               </button>
               <button className="stats-missed-btn">
@@ -16,17 +19,17 @@ function Stats() {
             </div>
           </div>
 
-          <div className="stats-pot-text">
-            <span className="stats-pot-title">
+          <div className="stats-text">
+            <span className="stats-title">
               Pot Success
             </span>
           </div>
 
-          <div className="stats-pot-second-player">
-            <span className="stats-pot-second-player-info">
+          <div className="stats-player">
+            <span className="stats-player-info">
               0% (0/0)
             </span>
-            <div className="stats-pot-second-player-btns">
+            <div className="stats-player-btns">
               <button className="stats-potted-btn">
               </button>
               <button className="stats-missed-btn">
@@ -34,6 +37,126 @@ function Stats() {
             </div>
           </div>
         </div>
+
+        {/* LONG POT SUCCESS */}
+        <div className="stats-card">
+          <div className="stats-player">
+            <span className="stats-player-info">
+              0% (0/0)
+            </span>
+            <div className="stats-player-btns">
+              <button className="stats-potted-btn">
+              </button>
+              <button className="stats-missed-btn">
+              </button>
+            </div>
+          </div>
+
+          <div className="stats-text">
+            <span className="stats-title">
+              Long Pot Success
+            </span>
+          </div>
+
+          <div className="stats-player">
+            <span className="stats-player-info">
+              0% (0/0)
+            </span>
+            <div className="stats-player-btns">
+              <button className="stats-potted-btn">
+              </button>
+              <button className="stats-missed-btn">
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* REST POT SUCCESS */}
+        <div className="stats-card">
+          <div className="stats-player">
+            <span className="stats-player-info">
+              0% (0/0)
+            </span>
+            <div className="stats-player-btns">
+              <button className="stats-potted-btn">
+              </button>
+              <button className="stats-missed-btn">
+              </button>
+            </div>
+          </div>
+
+          <div className="stats-text">
+            <span className="stats-title">
+              Rest Pot Success
+            </span>
+          </div>
+
+          <div className="stats-player">
+            <span className="stats-player-info">
+              0% (0/0)
+            </span>
+            <div className="stats-player-btns">
+              <button className="stats-potted-btn">
+              </button>
+              <button className="stats-missed-btn">
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* SAFETY SUCCESS */}
+        <div className="stats-card">
+          <div className="stats-player">
+            <span className="stats-player-info">
+              0% (0/0)
+            </span>
+            <div className="stats-player-btns">
+              <button className="stats-potted-btn">
+              </button>
+              <button className="stats-missed-btn">
+              </button>
+            </div>
+          </div>
+
+          <div className="stats-text">
+            <span className="stats-title">
+              Safety Success
+            </span>
+          </div>
+
+          <div className="stats-player">
+            <span className="stats-player-info">
+              0% (0/0)
+            </span>
+            <div className="stats-player-btns">
+              <button className="stats-potted-btn">
+              </button>
+              <button className="stats-missed-btn">
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="stats-card">
+          <div className="stats-player">
+            <span className="stats-player-info all-potted-balls-info">
+              0
+            </span>
+          </div>
+
+          <div className="stats-text">
+            <span className="stats-title">
+              All Potted Balls
+            </span>
+          </div>
+
+          <div className="stats-player">
+            <span className="stats-player-info all-potted-balls-info">
+              0
+            </span>
+          </div>
+        </div>
+
       </div>
     </section>
   );
