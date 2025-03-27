c93879ca8bfaaf27f31c8f423f0eb03504ea1641
Farhad Sadykov <f.sadykov@yahoo.com>
2025-03-27 22:24:48 +0300

    edited App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import Header from "./components/Header";
 import NamesForm from "./components/NamesForm";
-import Players from "./components/Players";
-import Stats from "./components/Stats";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <NamesForm />
-      {/* <Players />
-      <Stats />
-      <Footer /> */}
     </div>
   );
 }
