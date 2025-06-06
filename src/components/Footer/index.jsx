4302cc1c1c867f78f3c580fc3b5a8b948dba2b10
Farhad Sadykov <f.sadykov@yahoo.com>
2025-06-06 18:05:39 +0300

    added google play link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,6 +3,8 @@ function Footer() {
     <footer className="footer">
       <div className="container">
         <div className="footer-wrap">
+          <a href="https://play.google.com/store/apps/details?id=ru.homeofsnooker.sscapp" className="footer-google-play-link" target="_blank">
+          </a>
           <a href="https://vk.com/snookernews" className="footer-copyright-link" target="_blank" rel="noopener noreferrer">
             <span className="footer-copyright-text">
               2025 © Home of Snooker
